13a7af6fc3c10a18f08f30470e4ad2b01a8444a9
Victor Carter <Victor.A.York@gmail.com>
2015-10-22 09:32:11 -0500

    Victor: Added a playFirst method to SongQueue. - commit by victor.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,12 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function (){
+  initialize: function () {
+  },
+
+  // added by vic. Doesn't do anything yet...
+  playFirst: function () {
+
   }
 
 });
